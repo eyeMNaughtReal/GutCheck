2b2ad66d964a9d42b0481cb5212d7cdb13563894
eyemnaughtreal <mykid8yours@gmail.com>
2025-07-09 19:22:59 -0400

    initial Xcode project setup

---
+++ GutCheck/GutCheck/GutCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  GutCheckApp.swift
+//  GutCheck
+//
+//  Created by Mark Conley on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GutCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
