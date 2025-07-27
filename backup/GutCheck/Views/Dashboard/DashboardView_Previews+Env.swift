24fcd703b3d8bda1d8f3a711972f1adea14bb442
Mark Conley <mykid8yours@gmail.com>
2025-07-27 14:13:13 -0400

    Delete backup directory

--- backup/GutCheck/Views/Dashboard/DashboardView_Previews+Env.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-// This preview injects a dummy AuthService so @EnvironmentObject is satisfied
-#Preview {
-    DashboardView()
-        .environmentObject(AuthService())
-}
--- backup/GutCheck/Views/Meal/FoodSearchViewModifier.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-extension FoodSearchView {
-    func onSelectFood(_ action: @escaping (FoodItem) -> Void) -> some View {
-        var view = self
-        view.onSelect = action
-        return view
-    }
-}
